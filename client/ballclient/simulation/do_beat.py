57c258e32df1f8dd8b84bfd5bf49c8a59453ae74
chierqj <chieramiaoa@gmail.com>
2019-09-06 18:51:42 +0800

    躲鱼不改了，改下抓鱼 TAG

--- client/ballclient/simulation/do_beat.py
+++ client/ballclient/simulation/do_beat.py
@@ -15,9 +15,10 @@ from ballclient.simulation.my_action import Action
 class DoBeat(Action):
     def __init__(self):
         super(DoBeat, self).__init__()
+        self.HAVE_RET_POINT = set()
 
     def init(self):
-        pass
+        self.HAVE_RET_POINT = set()
 
     '''
     match做条件判断相关
@@ -36,8 +37,8 @@ class DoBeat(Action):
                 continue
             if self.judge_in_vision(enemy.x, enemy.y, player.x, player.y):
                 return True
-            dis = mLegStart.get_short_length(
-                enemy.x, enemy.y, player.x, player.y)
+            # dis = mLegStart.get_short_length(
+            #     enemy.x, enemy.y, player.x, player.y)
             # if dis <= 5:
             #     return True
         return False
@@ -126,7 +127,62 @@ class DoBeat(Action):
     逃跑
     '''
 
+    # 先看看能不能逃脱视野追踪
+    def pre_escape_vision(self, player):
+        uid = mLegStart.get_cell_id(player.x, player.y)
+        sons = mLegStart.SONS.get(uid, None)
+        if True == self.error_no_sons(sons, uid):
+            return False
+        for mv, nx, ny in sons:
+            vid = mLegStart.get_cell_id(nx, ny)
+            if False == self.match_self_can_say(nx, ny):
+                continue
+            if True == self.match_father_self_can_not_say(nx, ny):
+                continue
+            if True == self.match_wormhole_enemy_can_say(nx, ny):
+                continue
+            if True == self.match_enemy_can_say(nx, ny):
+                continue
+            player.move = mv
+            self.record_detial(player, "通过视野逃跑")
+            return True
+        return False
+
+    def get_sorted_sons(self, sons):
+        def cmp(s1, s2):
+            if False == self.match_enemy_can_say(s1[1], s1[2]):
+                return -1
+            if False == self.match_enemy_can_say(s2[1], s2[2]):
+                return 1
+            return 0
+        import copy
+        new_sons = copy.deepcopy(sons)
+        new_sons = sorted(new_sons, cmp)
+        return new_sons
+
+    def get_can_say_enemy(self):
+        can_say_enemy_num = 0
+        for k, enemy in othPlayers.iteritems():
+            if enemy.visiable == False:
+                continue
+            can_say_enemy_num += 1
+        return can_say_enemy_num
+
+    def add_action(self, player):
+        uid = mLegStart.get_cell_id(player.x, player.y)
+        sons = mLegStart.SONS.get(uid, None)
+        if True == self.error_no_sons(sons, uid):
+            return
+        for mv, nx, ny in sons:
+            if mv == player.move:
+                vid = mLegStart.get_cell_id(nx, ny)
+                self.HAVE_RET_POINT.add(vid)
+                return
+
     def escape(self, player):
+        # if True == self.pre_escape_vision(player):
+        #     return
+
         move_distance = dict()
         move_distance[""] = dict()
         move_distance["left"] = dict()
@@ -134,22 +190,21 @@ class DoBeat(Action):
         move_distance["up"] = dict()
         move_distance["down"] = dict()
 
-        can_say_enemy_num = 0
-        for k, enemy in othPlayers.iteritems():
-            if enemy.visiable == False:
-                continue
-            can_say_enemy_num += 1
+        mLogger.info("******************************************************")
+        can_say_enemy_num = self.get_can_say_enemy()
 
-        flag = False
         def bfs1():
+            mLogger.info("[开始 bfs1]")
             q = Queue.Queue()
             vis = set()
             start = mLegStart.get_cell_id(player.x, player.y)
             q.put(("", start, 0))
             vis.add(start)
+            flag = False
             while False == q.empty():
                 move, uid, ustep = q.get()
                 errx, erry = mLegStart.get_x_y(uid)
+                # mLogger.info("[ustep: {}; point: ({}, {})]".format(ustep, errx, erry))
                 if ustep >= 7:
                     break
                 if ustep >= 1:
@@ -169,25 +224,31 @@ class DoBeat(Action):
                 2. 视野看不到我就不走
                 3. 要走的点的父亲看不到，我就不走
                 '''
-                for mv, nx, ny in sons:
+                sort_sons = self.get_sorted_sons(sons)
+                for mv, nx, ny in sort_sons:
                     cell = mLegStart.get_cell_id(nx, ny)
                     if cell in vis:
                         continue
                     if True == self.judge_enemy_fast(nx, ny, ustep + 1):
                         continue
                     if ustep == 0:
+                        if cell in self.HAVE_RET_POINT:
+                            continue
                         if can_say_enemy_num < 4:
                             if False == self.match_self_can_say(nx, ny):
                                 continue
                             if True == self.match_father_self_can_not_say(nx, ny):
                                 continue
-                            if True == self.match_wormhole_enemy_fast(nx, ny, ustep + 1):
+                            if True == self.match_wormhole_enemy_fast(nx, ny, ustep):
                                 continue
                         move = mv
                     vis.add(cell)
                     q.put((move, cell, ustep + 1))
+            return flag
 
         def bfs2():
+            mLogger.info("[开始 bfs2]")
+
             q = Queue.Queue()
             vis = set()
             start = mLegStart.get_cell_id(player.x, player.y)
@@ -198,8 +259,6 @@ class DoBeat(Action):
                 errx, erry = mLegStart.get_x_y(uid)
                 if ustep >= 7:
                     break
-                if ustep >= 1:
-                    flag = True
                 st = move_distance[move].get(ustep, [])
                 st.append((errx, erry))
                 move_distance[move][ustep] = st
@@ -222,10 +281,14 @@ class DoBeat(Action):
                     if True == self.judge_enemy_fast(nx, ny, ustep + 1):
                         continue
                     if ustep == 0:
+                        if cell in self.HAVE_RET_POINT:
+                            continue
                         move = mv
                     vis.add(cell)
                     q.put((move, cell, ustep + 1))
-        if flag == False:
+
+        if False == bfs1():
+            mLogger.info("[尝试bfs1失败]")
             move_distance = dict()
             move_distance[""] = dict()
             move_distance["left"] = dict()
@@ -244,6 +307,7 @@ class DoBeat(Action):
                 if step > max_step or (step == max_step and count > max_count):
                     max_step, max_count, ret_move = step, count, move
         player.move = ret_move
+        self.add_action(player)
 
         self.record_detial(player, "逃跑")
         d = ["", "up", "down", "left", "right"]
@@ -270,7 +334,11 @@ class DoBeat(Action):
             cell = mLegStart.get_cell_id(power['x'], power['y'])
             if cell in used_power:
                 continue
+            if False == self.judge_in_vision(player.x, player.y, power['x'], power['y']):
+                continue
             power_area.add(cell)
+
+        can_say_enemy_num = self.get_can_say_enemy()
 
         q = Queue.Queue()
         vis = set()
@@ -284,7 +352,9 @@ class DoBeat(Action):
             errx, erry = mLegStart.get_x_y(uid)
             if uid in power_area:
                 player.move = move
+                self.add_action(player)
                 used_power.add(uid)
+                self.record_detial(player, "能量")
                 return True
             sons = mLegStart.SONS.get(uid, None)
             if True == self.error_no_sons(sons, uid):
@@ -300,9 +370,11 @@ class DoBeat(Action):
                 if cell in vis:
                     continue
                 if ustep == 0:
-                    if True == self.judge_suddenly_dead(nx, ny):
+                    if cell in self.HAVE_RET_POINT:
                         continue
-                    if True == self.match_enemy_can_say(nx, ny):
+                    if True == self.judge_suddenly_dead(nx, ny) and can_say_enemy_num < 4:
+                        continue
+                    if True == self.match_enemy_can_say(nx, ny) and can_say_enemy_num < 4:
                         continue
                     move = mv
                 vis.add(cell)
@@ -319,8 +391,16 @@ class DoBeat(Action):
                 return
             for mv, nx, ny in sons:
                 if mv == player.move:
-                    vid = mLegStart.get_cell_id(nx, ny)
-                    have_vision_count.add(vid)
+                    width, height = mLegStart.width, mLegStart.height
+                    vision = mLegStart.msg['msg_data']['map']['vision']
+                    x1, y1 = max(0, nx - vision), max(0, ny - vision)
+                    x2 = min(width - 1, nx + vision)
+                    y2 = min(height - 1, ny + vision)
+
+                    for i in range(x1, x2 + 1):
+                        for j in range(y1, y2 + 1):
+                            cell = mLegStart.get_cell_id(i, j)
+                            have_vision_count.add(cell)
                     return
 
         players = []
@@ -336,6 +416,8 @@ class DoBeat(Action):
                 continue
             players.append(player)
 
+        can_say_enemy_num = self.get_can_say_enemy()
+
         all_enums = self.get_all_enums(players)
         max_count, ret_enum = None, None
         for enum in all_enums:
@@ -343,10 +425,14 @@ class DoBeat(Action):
             vision_count = copy.deepcopy(have_vision_count)
             flag = True
             for pid, em, ex, ey in enum:
-                if True == self.judge_suddenly_dead(ex, ey):
+                eid = mLegStart.get_cell_id(ex, ey)
+                if eid in self.HAVE_RET_POINT:
                     flag = False
                     break
-                if True == self.match_enemy_can_say(ex, ey):
+                if True == self.judge_suddenly_dead(ex, ey) and can_say_enemy_num < 4:
+                    flag = False
+                    break
+                if True == self.match_enemy_can_say(ex, ey) and can_say_enemy_num < 4:
                     flag = False
                     break
                 width, height = mLegStart.width, mLegStart.height
@@ -384,12 +470,18 @@ class DoBeat(Action):
                 if max_count == None or len(vision_count) >= max_count:
                     max_count, ret_enum = len(vision_count), enum
 
+        if ret_enum == None:
+            mLogger.warning("[没有enum]")
+            return
+
         mLogger.info("[max_count: {}; ret_enum: {}]".format(
             max_count, ret_enum))
         for pid, em, ex, ey in ret_enum:
             mPlayers[pid].move = em
+            self.add_action(mPlayers[pid])
 
     def do_excute(self):
+        self.HAVE_RET_POINT.clear()
         for k, player in mPlayers.iteritems():
             if player.sleep == True:
                 continue
